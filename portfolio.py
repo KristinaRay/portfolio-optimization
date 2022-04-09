9303e11055194df810fbc7c8ddb8a4ab82508f32
truth.139 <truth.139@gmail.com>
2022-04-09 22:51:14 +0400

    add files

--- portfolio.py
+++ portfolio.py
@@ -8,25 +8,32 @@ seed_everything()
 NUM_PORTFOLIOS = 50000
 stock_data = pd.read_csv("data/stock_prices.csv", parse_dates=True, index_col="Date")
 
+def main():
+    # Expected returns of the assets
+    company_returns, covariance_matrix = stock_returns(stock_data)
 
-# Expected returns of the assets
-company_returns, covariance_matrix = stock_returns(stock_data)
+    minimum_volatility = min_volatility(company_returns, covariance_matrix)
 
-minimum_volatility = min_volatility(company_returns, covariance_matrix)
-print(f"Shares in the minimum volatility portfolio \n{minimum_volatility}")
+    maximum_sharpe_ratio = max_sharpe_ratio(company_returns, covariance_matrix)
 
-maximum_sharpe_ratio = max_sharpe_ratio(company_returns, covariance_matrix)
-print(f"Shares in the maximum sharpe ratio portfolio \n{maximum_sharpe_ratio}")
+    efficient_portfolio_return = efficient_return(company_returns, covariance_matrix)
 
-efficient_portfolio_return = efficient_return(company_returns, covariance_matrix)
-print(f"Shares in efficient return portfolio \n{efficient_portfolio_return}")
+    maximum_diversification = max_diversification(company_returns, covariance_matrix, long_only=True)
 
-maximum_diversification = max_diversification(company_returns, covariance_matrix, long_only=True)
-print(f"Shares in diversified portfolio \n{maximum_diversification}")
+    monte_carlo_min_volatility, monte_carlo_sharpe_ratio = monte_carlo_optimisation(company_returns, covariance_matrix, NUM_PORTFOLIOS)
 
-monte_carlo_min_volatility, monte_carlo_sharpe_ratio = monte_carlo_optimisation(company_returns, covariance_matrix, NUM_PORTFOLIOS)
-print(f"Portfolio return with minimal volatility after Monte Carlo Optimization {monte_carlo_min_volatility['Returns']:.3f}")
-print('Shares ', [round(x, 3) for x in monte_carlo_min_volatility])
-maximum_sharpe_ratio = max_sharpe_ratio(company_returns, covariance_matrix)
-print(f"Portfolio return with Maximum Sharpe ratio after Monte Carlo Optimization {monte_carlo_sharpe_ratio['Returns']:.3f}")
-print('Shares ', [round(x, 3) for x in monte_carlo_sharpe_ratio])
+    columns = ['Asset allocation']+list(stock_data.columns)
+
+    result = pd.DataFrame([['Minimum Volatility'] + minimum_volatility,
+                          ['Maximum Sharpe Ratio'] + maximum_sharpe_ratio,
+                          ['Efficien Portfolio Return'] + efficient_portfolio_return,
+                          ['Maximum Diversification'] + maximum_diversification,
+                          ['Monte Carlo Minimum Volatility'] + [round(x, 3) for x in monte_carlo_min_volatility][2:],
+                          ['Monte Carlo Maximum Sharpe Ratio'] + [round(x, 3) for x in monte_carlo_sharpe_ratio][2:]],
+                          columns=columns)
+    print('Saving the result to ./result.csv')
+    result.to_csv('./result.csv')
+    # print(result)
+
+if __name__ == "__main__":
+    main()
